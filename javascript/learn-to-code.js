37b196060aba159f349a1bcfebd41224b08992f2
Daniel Winship <danielwinship@icloud.com>
2018-12-26 16:00:35 -0500

    built out pecentage calculator

--- javascript/learn-to-code.js
+++ javascript/learn-to-code.js
@@ -1,4 +1,19 @@
-if (1 == 1) {
-    console.log("we should see this");
-}
+var numField1 = document.getElementById("numField1");
+var numField2 = document.getElementById("numField2");
+var resultField = document.getElementById("resultField");
+var form = document.getElementById("xIsWhatPercentofY");
 
+form.addEventListener("submit", function(event) {
+    
+    if (!numField1.value || !numField2.value) {
+        alert("Please enter value in the fields");
+    } else {
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+        var result = x / y;
+        var percent = result * 100;
+        resultField.innerText = "Result: " + percent + "%";
+        event.preventDefault();
+    }
+    
+});
